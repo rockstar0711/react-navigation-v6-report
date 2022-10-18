33fd0a0e3d053c361074574adb30b6b451eab1be
Rockstar0711 <happpybopy123@outlook.com>
2022-10-19 02:56:34 +0300

    Create navigation-update-1.js

---
+++ navigation-update-1.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+  HOME,
+} from '../constants/routeNames';
+import Home from '../screens/Home';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { View } from 'react-native';
+
+const HomeNavigator = () => {
+  const HomeStack = createNativeStackNavigator();
+
+  return (
+    <>
+      <HomeStack.Navigator initialRouteName={HOME}>
+        <HomeStack.Screen
+          name={HOME}
+          component={Home}
+          options={{
+            headerTransparent: true,
+            headerTitleAlign: 'center',
+            headerLeft: () => (
+              <View style={{ width: 30, height: 30, backgroundColor: 'blue' }} />
+            ),
+            headerTitle: () => (
+              <View
+                style={{
+                  flex: 1,
+                  display: 'flex',
+                  flexDirection: 'row',
+                  justifyContent: 'center',
+                  flexWrap: 'wrap',
+                }}>
+                <View style={{ width: 30, height: 30, backgroundColor: 'red' }} />
+              </View>
+            ),
+            headerRight: () => (
+              <View style={{ width: 30, height: 30, backgroundColor: 'green' }} />
+            ),
+          }}></HomeStack.Screen>
+
+      </HomeStack.Navigator>
+    </>
+  );
+};
+
+export default HomeNavigator;
